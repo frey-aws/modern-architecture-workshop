847d64014406ac48df6bf58a78a814f12ee9f605
Dennis Frey <freydenn@amazon.com>
2016-10-20 01:02:15 -0400

    Remove sql credentials

--- lambda-to-sql/util/encrypt.js
+++ lambda-to-sql/util/encrypt.js
@@ -1,10 +1,10 @@
 var NodeRSA = require('node-rsa');
 var fs = require('fs');
-var key = new NodeRSA({b: 2048});
 
-var text = "mssql://www.user:aUXxafvqQ5ntpbNZ23CkzXbv@52.0.217.249:50002/WideWorldImporters";
-
-var encrypted = key.encrypt(text, 'base64', 'utf8');
-fs.writeFile('../credential/sql.txt', encrypted, function() {        
-    fs.writeFile('../credential/key.txt', key.exportKey("private"), function() {});
-});
+fs.readFile("sql.txt", function(err, text) {
+    var key = new NodeRSA({b: 2048});
+    var encrypted = key.encrypt(text, 'base64', 'utf8');
+    fs.writeFile('../credential/sql.txt', encrypted, function() {        
+        fs.writeFile('../credential/key.txt', key.exportKey("private"), function() {});
+    });
+})
